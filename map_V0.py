2654662a24cf8cdf3847879c85dd1041b2ae7018
bgeraci714 <bgspiral@Blakes-MacBook-Pro.local>
2016-04-20 16:18:29 -0400

    working on move functions
    Testing out move functions

--- map_V0.py
+++ map_V0.py
@@ -1,9 +1,13 @@
 class Map(object):
     """Creates map object (length, height)"""
-    def __init__(self, sizeY, sizeX):
+    def __init__(self, sizeY, sizeX, charSym = "S", startPosY = 0, startPosX = 0):
         
         self.sizeX = sizeX
         self.sizeY = sizeY
+        self.charSym = charSym
+        self.position = [startPosY,startPosX]
+        self.charY = startPosY
+        self.charX = startPosX
 
         ## create empty list
         self.grid = []
@@ -12,6 +16,31 @@ class Map(object):
         for x in self.grid: ## for each empty list
             for y in range(self.sizeY): ## for the num of times perscribed by sizeY
                 x.append("_") ##add char sizeY times to the sub list within x
+
+    ## move functions
+    def move_up(self):
+        self.position[0] -= 1
+        return self.position
+
+    def move_down(self):
+        self.position[0] += 1
+        return self.position
+
+    def move_left(self):
+        self.position[1] -= 1
+        return self.position
+
+    def move_right(self):
+        self.position[1] += 1
+        return self.position
+        
+    ##{"1":"Up", "2":"Right","3":"Left","4":"Down"}
+    self.moves = {"1":move_up(), "2":move_right(),
+             "3":move_left(), "4":move_down()}
+    movesList = []
+    for move in moves:
+        movesList.append(move)
+
 
     def isValid(self, posY, posX):
         if posY < 0 or posX < 0:
@@ -26,6 +55,7 @@ class Map(object):
             for y in range (self.sizeY):
                 print(self.grid[x][y], end=" ")
             print()
+            
     def populateTiles(self, numTiles):
         import random
         
@@ -34,12 +64,30 @@ class Map(object):
             randomY = random.randrange(self.sizeY)
             self.grid[randomX][randomY] = str(tile)
 
-    def changeTile (self, posY, posX, tile="t"):
+    def changeTile(self, posY, posX, tile="t"):
         """Change specific tile (Y, X, tile) """
         if self.isValid(posY, posX):
             self.grid[posY][posX] = tile
         else:
             print("Not a valid position on the map")
+'''
+    def move(self):
+        tempMoveMenu = "Choose a movement: \n\t1.Up\n\t2.Right\n\t3.Left\n\t4.Down\n"
+
+        validMove = False 
+
+        movement = None
+        while not validMove: 
+            while movement not in self.movesList:
+                movement = input(tempMoveMenu)
+            self.moves[movement]
+            possibleMove = self.position
+            if self.isValid(
+            
+        self.moves[movement]
+'''        
+
+    
     
             
 def main():
