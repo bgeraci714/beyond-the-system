7b168e00f83454c7dec6c5dd70a580e1de5aca4b
bgeraci714 <bgeraci851@hcc.edu>
2016-05-02 18:51:40 -0400

    added comment to game file.

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 import map
 
+"""
+Contains everything from map which is the true core to the game
+right now. This will be the storage space for main() when things
+are all coming together. 
+"""
+
