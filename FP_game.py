f4613b96a8de08c2e0634a4cfe612ea4a16ff0a4
bgeraci714 <bgeraci851@hcc.edu>
2016-05-10 17:37:02 -0400

    clean up

--- FP_game.py
+++
@@ -1,8 +0,0 @@
-import map
-
-"""
-Contains everything from map which is the true core to the game
-right now. This will be the storage space for main() when things
-are all coming together. 
-"""
-
